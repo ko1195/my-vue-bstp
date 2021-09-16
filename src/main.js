f86c7b9a3a81b8df8653630eee4872cca4958b05
ko1195 <ko1195@gmail.com>
2021-09-17 03:24:44 +0700

    first

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+import 'intersection-observer' // Optional
+
+//import Vue from 'vue'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+createApp.use(BootstrapVue);
+createApp.config.productionTip = false
+
+ 
 createApp(App).use(store).use(router).mount('#app')
